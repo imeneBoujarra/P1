023743ed572ff460ea1dce9d02f554d6d2098f7d
Skyness05 <aziz.marzouk.4015@gmail.com>
2024-05-24 00:55:02 +0200

    first_commit
    25/05/2024

---
+++ test2/test2/Data/Models/Checklist.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Data.Models
+{
+    public class Checklist
+    {
+        [Key]
+        public int? Id_checklist { get; set; }
+
+        // Define URL paths for images
+        [MaxLength(255)] 
+        public string HeatPictureUrl { get; set; }
+
+        [MaxLength(255)] 
+        public string SwitchersPictureUrl { get; set; }
+
+        [MaxLength(255)]
+        public string Backbone { get; set; }
+
+        [MaxLength(255)]
+        public string Ventulation { get; set; }
+
+        [MaxLength(255)]
+        public string Security { get; set; }
+
+        [MaxLength(255)]
+        public string Storage { get; set; }
+    }
+}
---
+++ test2/test2/Data/Models/ServerRoom.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+
+namespace Test.Data.Models
+{
+    public class ServerRoom
+    {
+        [Key]
+        public int? Id_Room { get; set; }
+
+        public int Room_Number { get; set; }
+
+        public int Servers_Numbers { get; set; }
+
+        public int Machines { get; set; }
+
+
+    }
+}
